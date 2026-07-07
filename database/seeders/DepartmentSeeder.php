fdc7b4e5a4dc3a6eed758aabd784ff1ca1be4a42
Momo-html <aldwinbael424@gmail.com>
2026-07-07 11:47:13 +0800

    Add new departments to DepartmentSeeder

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -14,8 +14,46 @@ class DepartmentSeeder extends Seeder
     public function run(): void
     {
         foreach ([
-            ['department_name' => 'College of Computer Studies', 'code' => 'CCS'],
-            ['department_name' => 'College of Business', 'code' => 'CB'],
+            [
+                'department_name' => 'Bachelor of Arts in Political Science',
+                'code' => 'BA-POLS',
+            ],
+            [
+                'department_name' => 'Bachelor of Arts in Communication',
+                'code' => 'BA-COMM',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Psychology',
+                'code' => 'BS-PSYCH',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Medical Technology',
+                'code' => 'BS-MT',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Information Technology',
+                'code' => 'BS-IT',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Accountancy',
+                'code' => 'BS-A',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Accounting Information System',
+                'code' => 'BS-AIS',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Business Administration',
+                'code' => 'BS-BA',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Hospitality Management',
+                'code' => 'BS-HM',
+            ],
+            [
+                'department_name' => 'Bachelor of Science in Tourism Management',
+                'code' => 'BS-TM',
+            ],
         ] as $department) {
             Department::query()->updateOrCreate(
                 ['code' => $department['code']],
